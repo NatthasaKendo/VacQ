5cd45db914eb0e8f236ddedd6795d272ebc76ea1
NatthasaKendo <kendokeyword@gmail.com>
2022-03-31 16:12:07 +0700

    change max limitter

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(xss());
 //* Rate limiting
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 1 // limit each IP to 100 requests per windowMs
+  max: 100 // limit each IP to 100 requests per windowMs
 });
 app.use(limiter);
 
